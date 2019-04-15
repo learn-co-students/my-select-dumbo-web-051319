87e8a36f119435de470febbb78e9d80833519999
TL <nbotapp1@gmail.com>
2019-04-15 15:10:23 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,8 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+  new_array = []
+  array.each_with_index do |item, index|
+    new_array << array[index] if yield item
+  end
+  new_array
+
 end
