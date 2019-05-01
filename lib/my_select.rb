fd780b6a992223e29e1fe7ca26c6225dd489a96c
Abdul <awsayed@alaska.edu>
2019-05-01 03:07:44 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
+
 def my_select(collection)
- # your code here!
+
+	return "This block should not run!" if collection.length == 0
+
+	selected = []
+	i = 0
+	while i < collection.length
+		selected.push( collection[i] ) if ( yield(collection[i]) == true )
+		i += 1
+	end
+	return selected
+
 end
